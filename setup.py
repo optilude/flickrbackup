4582b180993674b9deeff0d1de1b5d5074af3f53
Martin Aspeli <optilude@gmail.com>
2012-12-30 22:00:36 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="flickrbackup",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     scripts=['flickrbackup.py'],
     install_requires=['flickrapi', 'threadpool'],
